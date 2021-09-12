fe57dc14ab4c53ee0a653982655a67ff8fc0a7f2
proguy18 <josephjoey.leonardi@gmail.com>
2021-09-12 17:45:53 +0700

    Fixed plane and triangle intersection

--- src/scene/primitives/Plane.cs
+++ src/scene/primitives/Plane.cs
@@ -43,10 +43,11 @@ namespace RayTracer
             }
 
             // compute d parameter using equation 2
-            double d = normal.Dot(center); 
+            // double d = normal.Dot(center); 
+            p010 = center - ray.Origin;
         
             // compute t (equation 3)
-            t = (normal.Dot(ray.Origin) + d) / denominator; 
+            t = (normal.Dot(p010)) / denominator; 
             // check if the triangle is in behind the ray
             if (t < 0) {
                 return null;
@@ -61,38 +62,6 @@ namespace RayTracer
             Vector3 incidentRay = ray.Origin + ray.Direction;
 
             return new RayHit(intersection, normalHit, incidentRay, material);
-            // // Write your code here...
-            // double t = 0;
-            // double denominator = ray.Direction.Dot(normal);
-            // Vector3 p010 = new Vector3(0, 0, 0);
-            
-            // // FIX THIS LATER
-            // if (denominator < double.Epsilon) {
-            //     // Vector from origin to a point
-            //     Vector3 p0l0 = center - ray.Origin;
-            //     t = p010.Dot(normal) / denominator;
-
-            //     // Intersection
-            //     if (t >= 0) 
-            //     {
-            //         Vector3 intersection = ray.Origin + ray.Direction * t;
-
-            //         // Calculate the normal to the point of intersection
-            //         Vector3 normalHit = (intersection + normal).Normalized();
-
-            //         // Calculates the incident ray
-            //         Vector3 incidentRay = ray.Origin + ray.Direction;
-
-            //         return new RayHit(intersection, normalHit, incidentRay, material);
-            //     }
-            //     // Intersection behind the ray
-            //     else 
-            //     {
-            //         return null;
-            //     }
-            // }
-
-            // return null;
         }
 
         /// <summary>
--- src/scene/primitives/Triangle.cs
+++ src/scene/primitives/Triangle.cs
@@ -36,131 +36,30 @@ namespace RayTracer
 
             // Obtain variables for the plane which the triangle resides in
             Vector3 side0 = v1 - v0;
-            Vector3 side1 = v2 - v1;
+            Vector3 side1 = v2 - v0;
 
             // Normal of the plane
             Vector3 normal = side0.Cross(side1);
-            
-            double t = 0;
-            double denominator = ray.Direction.Dot(normal);
-            Vector3 p010 = new Vector3(0, 0, 0);
 
-            // If ray is not parallel to the plane, intersection occurs.
-            if (Math.Abs(denominator) < double.Epsilon) {
-                return null;
+            double det = -ray.Direction.Dot(normal);
+            double invdet = 1.0/det;
+            Vector3 AO  = ray.Origin - v0;
+            Vector3 DAO = AO.Cross(ray.Direction);
+            double u =  side1.Dot(DAO) * invdet;
+            double v = -side0.Dot(DAO) * invdet;
+            double t =  AO.Dot(normal)  * invdet; 
+            if(det >= double.Epsilon && t >= 0.0 && u >= 0.0 && v >= 0.0 && (u+v) <= 1.0) {
+                Vector3 intersection = ray.Origin + ray.Direction * t;
+
+                // Calculate the normal to the point of intersection
+                Vector3 normalHit = (intersection + normal).Normalized();
+
+                // Calculates the incident ray
+                Vector3 incidentRay = ray.Origin + ray.Direction;
+
+                return new RayHit(intersection, normalHit, incidentRay, material);
             }
-
-            // compute d parameter using equation 2
-            double d = normal.Dot(v0); 
-        
-            // compute t (equation 3)
-            t = (normal.Dot(ray.Origin) + d) / denominator; 
-            // check if the triangle is in behind the ray
-            if (t < 0) {
-                return null;
-                } // the triangle is behind 
-        
-            Vector3 intersection = ray.Origin + ray.Direction * t;
-
-            // Calculate the normal to the point of intersection
-            Vector3 normalHit = (intersection + normal).Normalized();
-
-            // Calculates the incident ray
-            Vector3 incidentRay = ray.Origin + ray.Direction;
-
-            // Step 2: inside-outside test
-            Vector3 C; // vector perpendicular to triangle's plane 
-        
-            // edge 0
-            Vector3 edge0 = v1 - v0; 
-            Vector3 vp0 = intersection - v0; 
-            C = edge0.Cross(vp0); 
-            if (normal.Dot(C) < 0) {
-              return null; // intersection is on the right side  
-            } 
-        
-            // edge 1
-            Vector3 edge1 = v2 - v1; 
-            Vector3 vp1 = intersection - v1; 
-            C = edge1.Cross(vp1); 
-            if (normal.Dot(C) < 0) {
-               return null; // intersection is on the right side 
-            }  
-        
-            // edge 2
-            Vector3 edge2 = v0 - v2; 
-            Vector3 vp2 = intersection - v2; 
-            C = edge2.Cross(vp2); 
-            if (normal.Dot(C) < 0) {
-              return null; // intersection is on the right side  
-            }  
-
-            return new RayHit(intersection, normalHit, incidentRay, material);
-
-            // if (denominator < double.Epsilon) {
-                // Vector from origin to a point
-            //     Vector3 p0l0 = v0 - ray.Origin;
-                
-            //     t = p010.Dot(normal) / denominator;
-
-            //     // Intersection
-            //     if (t >= 0) 
-            //     {
-                    
-            //         Vector3 intersection = ray.Origin + ray.Direction * t;
-
-            //         // Calculate the normal to the point of intersection
-            //         Vector3 normalHit = (intersection + normal).Normalized();
-
-            //         // Calculates the incident ray
-            //         Vector3 incidentRay = ray.Origin + ray.Direction;
-
-            //         // Checks whether the point of intersection is inside the triangle
-            //         Vector3 C = new Vector3(0, 0, 0);
-
-            //         // Edge 0 
-            //         Vector3 edge0 = v1 - v0; 
-            //         Vector3 vp0 = intersection - v0; 
-            //         C = edge0.Cross(vp0);
-            //         if (normal.Dot(C) < 0) 
-            //         {
-            //             return null;
-            //         } 
-                    
-            //         // Edge 1
-            //         Vector3 edge1 = v2 - v1; 
-            //         Vector3 vp1 = intersection - v1; 
-            //         C = edge1.Cross(vp1);
-            //         if (normal.Dot(C) < 0) 
-            //         {
-            //             return null;
-            //         } 
-
-            //         // Edge 2
-            //         Vector3 edge2 = v0 - v2; 
-            //         Vector3 vp2 = intersection - v2; 
-            //         C = edge2.Cross(vp2); 
-            //         if (normal.Dot(C) < 0) 
-            //         {
-            //             return null;
-            //         } 
-
-                    
-
-            //         return new RayHit(intersection, normalHit, incidentRay, material);
-            //     }
-            //     // Intersection behind the ray
-            //     else 
-            //     {
-            //         return null;
-            //     }
-            // }
             return null;
-
-            
-
-
-            // return null;
         }
 
         /// <summary>
